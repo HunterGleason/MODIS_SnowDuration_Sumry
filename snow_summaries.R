1825a77b02f93083df599946c0bb76b6890a9023
HunterGleason <hunter.gleason@gov.bc.ca>
2019-12-30 14:55:21 -0800

    Finalized annual analysis, tweaked NRD ggplot

--- snow_summaries.R
+++ snow_summaries.R
@@ -13,6 +13,7 @@ library(tidyverse)
 library(bcmaps)
 library(sf)
 library(boot)
+library(gridExtra)
 
 ##############################################################################################
 # Get BC Natural Resource Districts (NRD) and intersect with M*D10A1 snow duration samples from Bevignton et al. (2018)
@@ -21,12 +22,15 @@ library(boot)
 set.seed(101)
 
 #Get BC NRD as sf object
-nrd <- nr_districts()
+nrd <- nr_districts() %>%
+  mutate(dname_shrt = str_sub(DISTRICT_NAME,1,-27),rnum = row_number())
 
-
-nrd_plt <- ggplot() + geom_sf(data = nrd, aes( fill = ORG_UNIT))+labs(fill="Org. Unit")
+nrd_plt <- ggplot() + geom_sf(data = nrd)+geom_sf_label(data=nrd,aes(label = rnum)) + labs(y='Lat.', x = 'Lon.')
 nrd_plt
-ggsave(filename = 'nrd_map',plot = nrd_plt ,device = 'png',path = '~/Dropbox/FLNRO/Projects/snow_summaries_note/Figures/Maps/',width=8.5,height = 8.5,units = c('in'))
+  ggsave(filename = 'nrd_map',plot = nrd_plt ,device = 'png',path = '~/Dropbox/FLNRO/Projects/snow_summaries_note/Figures/Maps/',width=8.5,height = 8.5,units = c('in'))
+
+
+write_csv(as.data.frame(nrd %>% select(rnum,dname_shrt))[,c(1:2)] , path = '~/Dropbox/FLNRO/Projects/snow_summaries_note/Tables/legend.csv')
 
 
 # Bevington et al. (2018) M*D10A1 Snow Duration Samples (local copy)
@@ -34,8 +38,7 @@ sd_samp_pts<-st_read('~/Dropbox/FLNRO/Projects/snow_summaries_note/SD_Random_Sam
 
 #Join the SD samples to the NR Districts via spatial intersection 
 sd_by_nrd <- sd_samp_pts %>%
-  st_join(nrd, join = st_intersects) %>%
-  mutate(dname_shrt = str_sub(DISTRICT_NAME,1,-27))
+  st_join(nrd, join = st_intersects)
 
 ##############################################################################################
 #Create Box-Plots for SDon, SDoff and SDdur by NRD, save copies to working directory.
@@ -152,18 +155,66 @@ ggsave(filename = 'sd_sddur_map',plot = sd_dur_sd_plt ,device = 'png',path = '~/
 #Anual Analysis 
 ##############################################################################################
 
-#Get mean and std. dev. of SD metrics by NRD
+
+#Generate boxplots of snow metrics by hydrologic year
+box_anual_sdon <- ggplot(sd_by_nrd, aes(x=year,group=year, y=sdon)) + geom_boxplot(outlier.size=.1) + theme_classic()+ labs(y="M*D10A1 Snow On Date (Days Since 1-Sep)", x = "")
+box_anual_sdon
+ggsave(filename = 'box_sdon_yrly',plot = box_anual_sdon,device = 'png',path = '~/Dropbox/FLNRO/Projects/snow_summaries_note/Figures/BoxPlots/',width=8.5,height = 8.5,units = c('in'))
+
+
+box_anual_sdoff <- ggplot(sd_by_nrd, aes(x=year,group=year, y=sdoff)) + geom_boxplot(outlier.size=.1) + theme_classic()+ labs(y="M*D10A1 Snow Off Date (Days Since 1-Sep)", x = "")
+box_anual_sdoff
+ggsave(filename = 'box_sdoff_yrly',plot = box_anual_sdoff,device = 'png',path = '~/Dropbox/FLNRO/Projects/snow_summaries_note/Figures/BoxPlots/',width=8.5,height = 8.5,units = c('in'))
+
+
+box_anual_sddur <- ggplot(sd_by_nrd, aes(x=year,group=year, y=sddur)) + geom_boxplot(outlier.size=.1) + theme_classic()+ labs(y="M*D10A1 Snow Duration (Days)", x = "")
+box_anual_sddur
+ggsave(filename = 'box_sddur_yrly',plot = box_anual_sddur,device = 'png',path = '~/Dropbox/FLNRO/Projects/snow_summaries_note/Figures/BoxPlots/',width=8.5,height = 8.5,units = c('in'))
+
+
+
+
+#Get mean and std. dev. of SD metrics by hydrologic year 
 sd_anual_sumry <- sd_by_nrd %>%
   group_by(year) %>%
   summarise(mean_sdon = mean(sdon, na.rm = TRUE), sd_sdon = sd(sdon, na.rm = TRUE),mean_sdoff = mean(sdoff, na.rm = TRUE), sd_sdoff = sd(sdoff, na.rm = TRUE),mean_sddur = mean(sddur, na.rm = TRUE), sd_sddur = sd(sddur, na.rm = TRUE))
 
+#Write summary table to working directory
+write_csv(sd_anual_sumry %>% as_tibble() %>% select(year,mean_sdon,sd_sdon,mean_sdoff, sd_sdoff, mean_sddur, sd_sddur), path = '~/Dropbox/FLNRO/Projects/snow_summaries_note/Tables/sumry_tab_yrly.csv')
 
 
-box_anual_sdon <- ggplot(sd_by_nrd, aes(x=year,group=year, y=sdon)) + geom_boxplot(outlier.size=.1) + theme_classic()+ labs(y="M*D10A1 Snow On Date (Days Since 1-Sep)", x = "")
-box_anual_sdon
 
-box_anual_sdoff <- ggplot(sd_by_nrd, aes(x=year,group=year, y=sdoff)) + geom_boxplot(outlier.size=.1) + theme_classic()+ labs(y="M*D10A1 Snow Off Date (Days Since 1-Sep)", x = "")
-box_sdoff
+##############################################################################################
+#Get boot straped CI's for summary stats 
+##############################################################################################
 
-box_anual_sddur <- ggplot(sd_by_nrd, aes(x=year,group=year, y=sddur)) + geom_boxplot(outlier.size=.1) + theme_classic()+ labs(y="M*D10A1 Snow Duration (Days)", x = "")
-box_anual_sddur
+
+
+sd_cis_mu_yrly<-sd_by_nrd %>%
+  group_by(year) %>%
+  summarise(sdon_lw = boot.ci(boot(data = sdon,statistic=mu,R=reps),type=c("basic"))$basic[4],
+            sdon_up = boot.ci(boot(data = sdon,statistic=mu,R=reps),type=c("basic"))$basic[5],
+            sdoff_lw = boot.ci(boot(data = sdoff,statistic=mu,R=reps),type=c("basic"))$basic[4],
+            sdoff_up = boot.ci(boot(data = sdoff,statistic=mu,R=reps),type=c("basic"))$basic[5],
+            sddur_lw = boot.ci(boot(data = sddur,statistic=mu,R=reps),type=c("basic"))$basic[4],
+            sddur_up = boot.ci(boot(data = sddur,statistic=mu,R=reps),type=c("basic"))$basic[5])
+
+#Write bootstrap CI's to working directory 
+write_csv(sd_cis_mu_yrly %>% as_tibble() %>% select(year,sdon_lw,sdon_up,sdoff_lw,sdoff_up,sddur_lw,sddur_up), path = '~/Dropbox/FLNRO/Projects/snow_summaries_note/Tables/boot_ci_mu_yrly.csv')
+
+
+
+#Map bootstrap function to compute std. dev. for each SD metric by NRD
+sd_cis_sdev_yrly<-sd_by_nrd %>%
+  group_by(year) %>%
+  summarise(sdon_lw = boot.ci(boot(data = sdon,statistic=sdev,R=reps),type=c("basic"))$basic[4],
+            sdon_up = boot.ci(boot(data = sdon,statistic=sdev,R=reps),type=c("basic"))$basic[5],
+            sdoff_lw = boot.ci(boot(data = sdoff,statistic=sdev,R=reps),type=c("basic"))$basic[4],
+            sdoff_up = boot.ci(boot(data = sdoff,statistic=sdev,R=reps),type=c("basic"))$basic[5],
+            sddur_lw = boot.ci(boot(data = sddur,statistic=sdev,R=reps),type=c("basic"))$basic[4],
+            sddur_up = boot.ci(boot(data = sddur,statistic=sdev,R=reps),type=c("basic"))$basic[5])
+
+#Write bootstrap CI's to working directory 
+write_csv(sd_cis_sdev_yrly %>% as_tibble() %>% select(year,sdon_lw,sdon_up,sdoff_lw,sdoff_up,sddur_lw,sddur_up), path = '~/Dropbox/FLNRO/Projects/snow_summaries_note/Tables/boot_ci_sdev_yrly.csv')
+
+
